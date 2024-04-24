86e30d39374986fa8fba51de932f1e4a8f1501d5
2016bits <1835154145@qq.com>
2024-04-24 08:14:44 +0800

    测试unbiased_model在CHEF二分类的效果

--- two_class_scripts/unbiased_model.py
+++ two_class_scripts/unbiased_model.py
@@ -145,9 +145,9 @@ def test(model, logger, test_loader):
 def main(args):
     # init logger
     if args.mode == "train":
-        log_path = args.log_path + "{}_class_unbiased_constraint_{}_claim_{}.log".format(args.num_classes, args.constraint_loss_weight, args.claim_loss_weight)
+        log_path = args.log_path + "{}_class_CHEF_unbiased_constraint_{}_claim_{}.log".format(args.num_classes, args.constraint_loss_weight, args.claim_loss_weight)
     elif args.mode == "test":
-        log_path = args.log_path + "test_unbiased_model.log"
+        log_path = args.log_path + "test_two_class_CHEF_unbiased_model.log"
     logger = log.get_logger(log_path)
 
     # load data
@@ -201,23 +201,23 @@ def main(args):
         train(args, model, train_loader, dev_loader, logger)
     acc, micro_f1, pre, recall, macro_f1 = test(model, logger, test_loader)
 
-    with open(args.test_results, 'a+') as f:
-        print("constraint_loss_weight: {}, claim_loss_weight: {}".format(args.constraint_loss_weight, args.claim_loss_weight), file=f)
-        print("         Accuracy: {:.3%}".format(acc))
-        print("       F1 (micro): {:.3%}".format(micro_f1), file=f)
-        print("Precision (macro): {:.3%}".format(pre), file=f)
-        print("   Recall (macro): {:.3%}".format(recall), file=f)
-        print("       F1 (macro): {:.3%}".format(macro_f1), file=f)
+    # with open(args.test_results, 'a+') as f:
+    #     print("constraint_loss_weight: {}, claim_loss_weight: {}".format(args.constraint_loss_weight, args.claim_loss_weight), file=f)
+    #     print("         Accuracy: {:.3%}".format(acc))
+    #     print("       F1 (micro): {:.3%}".format(micro_f1), file=f)
+    #     print("Precision (macro): {:.3%}".format(pre), file=f)
+    #     print("   Recall (macro): {:.3%}".format(recall), file=f)
+    #     print("       F1 (macro): {:.3%}".format(macro_f1), file=f)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--log_path", type=str, default='./logs/parameter2/')
-    parser.add_argument("--data_path", type=str, default="./data/improved_CHEF_2/[DATA].json")
-    parser.add_argument("--saved_model_path", type=str, default="./models/parameter2/two_unbiased_model_[constraint]_[claim].pth")
-    parser.add_argument("--test_results", type=str, default="./logs/test_result_unbiased_2_class.txt")
+    parser.add_argument("--log_path", type=str, default='./logs/')
+    parser.add_argument("--data_path", type=str, default="./data/processed/[DATA]_2.json")
+    parser.add_argument("--saved_model_path", type=str, default="./models/two_unbiased_CHEF_[constraint]_[claim].pth")
+    # parser.add_argument("--test_results", type=str, default="./logs/test_result_unbiased_2_class.txt")
 
     parser.add_argument("--cache_dir", type=str, default="./bert-base-chinese")
-    parser.add_argument("--checkpoint", type=str, default="./models/two_class_unbiased_model.pth")
+    parser.add_argument("--checkpoint", type=str, default="./models/two_unbiased_CHEF_0.004_0.5.pth")
 
     parser.add_argument("--num_sample", type=int, default=-1)
     parser.add_argument("--num_classes", type=int, default=2)
@@ -234,7 +234,7 @@ if __name__ == '__main__':
     # hyperparameters
     parser.add_argument("--seed", type=int, default=1111)
     parser.add_argument("--claim_loss_weight", type=float, default=0.5)
-    parser.add_argument("--constraint_loss_weight", type=float, default=0.008)
+    parser.add_argument("--constraint_loss_weight", type=float, default=0.004)
 
     args = parser.parse_args()
     main(args)
